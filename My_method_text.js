9e11233f8997b1860ced29eab174b411d4e1bb8f
Natalya <62794540+NatalyaKirsanova@users.noreply.github.com>
2020-06-11 10:56:43 +0300

    Add files via upload

---
+++ My_method_text.js
@@ -0,0 +1,34 @@
+function jQuery(selector, context = document) {
+    this.elements = Array.from(context.querySelectorAll(selector));
+    return this;
+}
+
+jQuery.prototype.each = function(fn) {
+    this.elements.forEach((element, index) => fn.call(element, element, index));
+    return this;
+}
+
+jQuery.prototype.click = function(fn) {
+    this.each(element => element.addEventListener('click', fn))
+    return this
+}
+const $ = (e) => new jQuery(e);
+
+$('button').click(e => console.log(e.target))
+
+
+//-----------------prototype.text--------------------------------------
+
+jQuery.prototype.text = function(newtext) {
+    if (newtext === undefined) {
+        this.each(element => element.textContent)
+    } else {
+        this.each(element => element.textContent = newtext)
+    }
+    return this;
+}
+
+
+//----------------------Проверка text-------------------------
+console.log($("h1").text().elements[0])
+console.log($(".ab").text("NEW_Text class_ab").elements[0])
